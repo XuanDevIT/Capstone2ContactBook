52643c1756424b130c35d91fa2c7be18e478858c
xuanle <xuanle@localhost>
2022-08-29 11:16:41 +0700

    commit API save Student

--- src/main/java/com/example/capstone/dto/NewDTO.java
+++ src/main/java/com/example/capstone/dto/NewDTO.java
@@ -10,7 +10,6 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class NewDTO {
-
 	private String title;
 	private String content;
 	private String shortDescription;
---
+++ src/main/java/com/example/capstone/entity/StudentEntity.java
@@ -0,0 +1,41 @@
+package com.example.capstone.entity;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "student")
+public class StudentEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long studentId;
+	
+	private String userName;
+	private String password;
+	private String fullName;
+	private String classStudent;
+	private int age;
+	private Date birthDay;
+	private String address;
+	private String phone;
+
+//	@ManyToMany
+//	@JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+//	private List<RoleEntity> roles = new ArrayList<>();
+
+}
